0e64746e52c46e966a756c30e5d27a049c376547
Aleksey <email>
2020-07-07 15:39:20 +0300

    User login/logout

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import App from './App'
 import rootReducer from './store/reducers/rootReducer'
+import { userLoggedIn } from './store/actions/user/auth'
 
 const store = createStore(
 	rootReducer,
 	composeWithDevTools(applyMiddleware(thunk))
 )
+
+if(localStorage.simpleTestsJWT) {
+	const user = { token: localStorage.simpleTestsJWT }
+	store.dispatch(userLoggedIn(user))
+}
 
 ReactDOM.render(
 	<React.StrictMode>
