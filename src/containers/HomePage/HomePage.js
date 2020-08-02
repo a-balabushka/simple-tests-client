83cfede26d8496fa1003cd4fe9436e0674c7a42c
Aleksey <email>
2020-08-02 17:57:23 +0300

    Home page adaptive
    - Create UI elements;
    - Add Home page style; 
    - Add Home page adaptive.

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,21 +1,44 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { logout } from '../../store/actions/user/auth'
+import TabTitle from '../../components/UI/TabTitle/TabTitle'
+import Button from '../../components/UI/Button/Button'
+import SocialLinks from '../../components/UI/SocialLinks/SocialLinks'
 
-const HomePage = ({ isAuth, logout }) => (
-	<div>
-		<h1>Home Page</h1>
-		{isAuth ? (
-			<button onClick={() => logout()}>Logout</button>
-		) : (
-			<div>
-				<Link to="/login">Login</Link> or <Link to="/signup">Sign Up</Link>
-			</div>
-		)}
-	</div>
-)
+import './HomePage.scss'
+import ContainerWithArt from '../../components/UI/ContainerWithArt/ContainerWithArt'
+
+const HomePage = ({ isAuth, logout }) => {
+	const whenLogin = <button onClick={() => logout()}>Logout</button>
+	const noLogin = (
+		<>
+			<Button to="/login" text="Log In" />
+			<Button to="/signup" text="Sign Up" />
+		</>
+	)
+
+	return (
+		<>
+			<TabTitle title="Welcome!" />
+			<ContainerWithArt>
+				<h1>Welcome to Simple Tests!</h1>
+				<p>
+					<span>Simple Tests</span> — Lorem ipsum dolor sit amet, consectetur
+					adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+					magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+					commodo consequat.
+				</p>
+				<div className="button-container">{isAuth ? whenLogin : noLogin}</div>
+				<SocialLinks />
+			</ContainerWithArt>
+		</>
+	)
+}
 
 HomePage.propTypes = {
 	isAuth: PropTypes.bool.isRequired,
